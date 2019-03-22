666ff41c91a43bd07c64bb6183de16294054aaa7
rtanubra <rdtanubrata@gmail.com>
2019-03-22 08:08:53 -0400

    working on fetch- twitter api

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ let stock_symbol = ""
 let company_name = ""
 const alphavantage_api_key ="P1IPHWHQ7R3CIHDT"
 const twitter_bearer = "AAAAAAAAAAAAAAAAAAAAAESX9gAAAAAAMaY%2FkPLVr%2FVvbVtKXy%2Brvce3SIk%3DP4Vw1WrkLpL6FwB3K9Uqg0nGK6lY48jNZz7ssdfsqBUTktC8Wb"
+let date_string = ""
+
+function getDateString(){
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth(); //January is 0!
+    const yyyy = today.getFullYear();
+    const months =["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    date_string = `${months[mm]} ${dd},${yyyy}`
+}
 
 function updateHomeNumbers(myJson){
-    const open = myJson["Global Quote"]["02. open"]
-    const close = myJson["Global Quote"]["05. price"]
-    const pct_change = (close-open)/close*100
-    console.log(open,close,pct_change)
+    const open = Math.round(myJson["Global Quote"]["02. open"] * 100) / 100
+    const close = Math.round(myJson["Global Quote"]["05. price"]*100)/100
+    const pct_change = Math.round((close-open)/close*100*1000)/1000
+    $(".home-open").text(`$${open}`)
+    $(".home-close").text(`$${close}`)
+    $(".home-pct").text(`%${pct_change}`)
+    $(".home-header").text(`${stock_symbol} pulled on ${date_string}`)
+    //console.log(open,close,pct_change)
 }
 
 function fetchAlphavantage(){
@@ -26,20 +40,41 @@ function fetchAlphavantage(){
     ).then(
         responseJson =>{
             updateHomeNumbers(responseJson)
-            console.log(responseJson)
+            //console.log(responseJson)
         }
     ).catch(err=>{
         console.log(err)
     })
 }
 
-function fetchNews(){
-    console.log(`Attempting to pull ${company_name} for news`)
+function urlExtend(base_url,params){
+    const queryItems = Object.keys(params)
+    const queryString = queryItems.map(key=>{
+        return `${key}=${params[key]}`
+    })
+    const finalUrl = `${base_url}?${queryString.join("&")}`
+    console.log(finalUrl)
+    return finalUrl
 }
 
 function fetchSocial(){
     console.log(`Attempting to pull ${stock_symbol} on social`)
+    const base_url = "https://api.twitter.com/1.1/search/tweets.json"
+    const  params = {
+        "q":`$${stock_symbol}`
+    }
+    const options = {
+    headers: new Headers({
+      "Authorization": `Bearer ${twitter_bearer}`})
+    };
+    const queryUrl = urlExtend(base_url,params)
 }
+
+function fetchNews(){
+    console.log(`Attempting to pull ${company_name} for news`)
+}
+
+
 
 function fetchRunner(){
     fetchAlphavantage()
@@ -62,5 +97,6 @@ function watch_submit(){
 function ready_fx(){
     console.log("Ready to watch some functions")
     watch_submit()
+    getDateString()
 }
 $(ready_fx)
